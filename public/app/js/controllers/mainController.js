8aced82bd3100590e8cca3cad6481327717c48a9
Lina Mazur <mazurhalinapost@gmail.com>
2017-04-18 15:08:53 +0000

    auth

--- public/app/js/controllers/mainController.js
+++ public/app/js/controllers/mainController.js
@@ -14,11 +14,13 @@ app.controller('MainController', function($location,  pollServices) {
 	
 	vm.pollFun = function(arg) {
 		if(arg == 'auth') {
-			var newWin = window.open("https://app-voting-jamaurz.c9users.io/auth/twitter", "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=500,width=400,height=400");
-			setTimeout(function() {
-		    	newWin.window.close();
-		    	window.location.reload();
-		    }, 8000);
+			var newWin = window.open("https://app-voting-jamaurz.c9users.io/auth/twitter");
+		
+			//var newWin = window.open("https://app-voting-jamaurz.c9users.io/auth/twitter", "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=500,width=400,height=400");
+			// setTimeout(function() {
+		 //   	newWin.window.close();
+		 //   	window.location.reload();
+		 //   }, 8000);
 		} else {
 			$location.path(arg);
 		}
